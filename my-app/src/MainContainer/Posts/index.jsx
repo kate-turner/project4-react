9220c0b39a225a8c3c818126eca23825a9f45410
Laurie Carano <lauriecarano@gmail.com>
2018-08-20 15:56:02 -0600

    redo on the post index page - added bootstrap

--- my-app/src/MainContainer/Posts/index.jsx
+++ my-app/src/MainContainer/Posts/index.jsx
@@ -6,27 +6,44 @@ import {
 
 
 
-   
-    
-  const Posts = (props) => {
 
-  const postList = props.posts.map((post, i ) => {
+
+const Posts = (props) => {
+
+  const postList = props.posts.map((post, i) => {
     console.log(post, ' post id')
     return (
-      <li key={post.id}> 
-        <h1>{post.title}</h1><br/>
-        <small>{post.date}</small><br/>
-        <small>{post.body}</small><br/>
-        <img src={post.img_url}/>
+      <Container className="container">
+        <Row>
+          <Col>
+            <Card>
+              <CardImg top width="auto" src={post.img_url} alt="" />
+              <CardBody>
+                <CardTitle>
+                  <h3 className="blog-title">{post.title}</h3>
+                </CardTitle>
+                <CardSubtitle>
+                  <div key={post.id}>
+                    <h6>{post.date}</h6>
+                  </div>
+                </CardSubtitle>
+                <CardText className="blog-body">
+                  {post.body}
+                </CardText>
+              </CardBody>
+            </Card>
+          </Col>
+        </Row>
         <button onClick={props.deletePost.bind(null, post.id)}>Delete</button>
         <button onClick={props.showModal.bind(null, post.id)}>Edit</button>
-    </li>
-    )  
+      </Container>
+
+    )
   })
-
-
-
-  return (
+      
+      
+      
+    return (
 
     <ul>
       {postList}
--- my-app/src/MainContainer/index.jsx
+++ my-app/src/MainContainer/index.jsx
@@ -170,68 +170,7 @@ class MainContainer extends Component {
 
         <Carousel />
 
-        <Container className="container">
-          <Row>
-            <Col>
-              <Card>
-                <CardImg top width="auto" src="https://i.imgur.com/1cNAbX6.jpg" alt="" />
-                <CardBody>
-                  <CardTitle>
-                    <h3 className="blog-title"><a href="this.post.title">The best shrimp EVER!</a></h3>
-                  </CardTitle>
-                  <CardSubtitle>
-                    <div key="this.post._id">
-                      <a href="#">July 4, 2018</a>
-                    </div>
-                  </CardSubtitle>
-                  <CardText className="blog-body">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer feugiat nisl id odio commodo aliquet. Maecenas lobortis, tellus sit amet ullamcorper tristique, risus libero aliquet velit, nec mollis purus purus eu velit. Suspendisse lacinia rutrum diam, nec viverra felis accumsan eu. Sed lacinia sagittis metus vitae malesuada. Maecenas mattis lacus elit. Vestibulum a fermentum metus, sit amet tincidunt metus. Donec sagittis sapien sit amet velit euismod eleifend. Sed ornare ante eros, eget facilisis nibh faucibus at. Nam ut augue vitae velit posuere scelerisque. Sed eleifend odio ac elementum gravida. Sed nisl risus, ullamcorper ut lacus quis, scelerisque iaculis sem. Donec ut quam interdum, viverra nunc vitae, elementum massa. Phasellus in lobortis nisl.
-                  </CardText>
-
-                </CardBody>
-              </Card>
-            </Col>
-            <Col>
-              <Card>
-                <CardImg top width="20%" src="https://i.imgur.com/P2tMdYj.jpg" alt="" />
-                <CardBody>
-                  <CardTitle>
-                    <h3 className="blog-title"><a href="this.post.title">Only good in public!</a></h3>
-                  </CardTitle>
-                  <CardSubtitle>
-                    <div key="this.post._id">
-                      <a href="#">August 3, 2018</a>
-                    </div>
-                  </CardSubtitle>
-                  <CardText className="blog-body">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer feugiat nisl id odio commodo aliquet. Maecenas lobortis, tellus sit amet ullamcorper tristique, risus libero aliquet velit, nec mollis purus purus eu velit. Suspendisse lacinia rutrum diam, nec viverra felis accumsan eu. Sed lacinia sagittis metus vitae malesuada. Maecenas mattis lacus elit. Vestibulum a fermentum metus, sit amet tincidunt metus. Donec sagittis sapien sit amet velit euismod eleifend. Sed ornare ante eros, eget facilisis nibh faucibus at. Nam ut augue vitae velit posuere scelerisque. Sed eleifend odio ac elementum gravida. Sed nisl risus, ullamcorper ut lacus quis, scelerisque iaculis sem. Donec ut quam interdum, viverra nunc vitae, elementum massa. Phasellus in lobortis nisl.
-                  </CardText>
-
-                </CardBody>
-              </Card>
-            </Col>
-
-            <Col>
-              <Card>
-                <CardImg top width="20%" src="https://i.imgur.com/xTelMtI.jpg" alt="" />
-                <CardBody>
-                  <CardTitle>
-                    <h3 className="blog-title"><a href="this.post.title">I was so sacred!</a></h3>
-                  </CardTitle>
-                  <CardSubtitle>
-                    <div key="this.post._id">
-                      <a href="#">September 3, 2017</a>
-                    </div>
-                  </CardSubtitle>
-                  <CardText className="blog-body">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer feugiat nisl id odio commodo aliquet. Maecenas lobortis, tellus sit amet ullamcorper tristique, risus libero aliquet velit, nec mollis purus purus eu velit. Suspendisse lacinia rutrum diam, nec viverra felis accumsan eu. Sed lacinia sagittis metus vitae malesuada. Maecenas mattis lacus elit. Vestibulum a fermentum metus, sit amet tincidunt metus. Donec sagittis sapien sit amet velit euismod eleifend. Sed ornare ante eros, eget facilisis nibh faucibus at. Nam ut augue vitae velit posuere scelerisque. Sed eleifend odio ac elementum gravida. Sed nisl risus, ullamcorper ut lacus quis, scelerisque iaculis sem. Donec ut quam interdum, viverra nunc vitae, elementum massa. Phasellus in lobortis nisl.
-                  </CardText>
-
-                </CardBody>
-              </Card>
-            </Col>
-          </Row>
-        </Container>
+        
       </Aux>
     );
   }
