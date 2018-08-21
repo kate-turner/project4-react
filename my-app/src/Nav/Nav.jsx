7d13f38a7221a5ef8f45abf9e2356c16e8f0d405
Laurie Carano <lauriecarano@gmail.com>
2018-08-21 14:01:18 -0600

    fixed width on the comment textarea

--- my-app/src/Nav/Nav.jsx
+++ my-app/src/Nav/Nav.jsx
@@ -37,15 +37,15 @@ import {
     return (
       <Aux >
      <Navbar className="navbar-main" color="light" light toggleable>
-          <NavbarBrand className="navbarbrand"  src="" href="/">Writer's Blog</NavbarBrand>
+          <NavbarBrand className="navbarbrand link" src="" href="/">Writer's Blog</NavbarBrand>
           <NavbarToggler onClick={this.toggle} />
           <Collapse isOpen={this.state.isOpen} navbar>
             <Nav className="ml-auto" navbar>
               <NavItem>
-                <NavLink className="navbarbrand" href="/">Home</NavLink>
+                <NavLink className="navbarbrand link" href="/">Home</NavLink>
               </NavItem>
               <NavItem>
-                <NavLink className="navbarbrand" href="/new">Create a Post</NavLink>
+                <NavLink className="navbarbrand link" href="/new">Create a Post</NavLink>
 
               </NavItem>
 
