4264466d3ee7bceed429cc4b29616f3215d5c036
amandamolde <amanda.molde@gmail.com>
2018-08-21 10:15:22 -0600

    Posts are now showing 2 in a row rather than 1 column

--- my-app/src/MainContainer/Posts/index.jsx
+++ my-app/src/MainContainer/Posts/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {
   Container, Row, Col, Card, CardImg, CardText, CardBody,
-  CardTitle, CardSubtitle, Button
+  CardTitle, CardSubtitle, Button, CardDeck
 } from 'reactstrap';
 import CreateComment from '../Comments/AddComment';
 import Comments from '../Comments.js'
@@ -17,35 +17,29 @@ const Posts = (props) => {
 
     // console.log(props.comments[0], ' this is props comments in Posts');
     return (
-      <Container className="container">
-        
-        <Row>
-          <Col className="column-posts" sm="4">
-            <Card>
-              <CardImg top width="auto" src={post.img_url} alt="" />
-              <CardBody>
-                <CardTitle>
-                  <h3 className="blog-title"><a href="../PostShowPage/PostShowPage.jsx"></a>{post.title}</h3>
-                </CardTitle>
-                <CardSubtitle>
-                  <div key={post.id}>
-                    <h6>{post.date}</h6>
-                  </div>
-                </CardSubtitle>
-                <CardText className="blog-body">
-                  {post.body}
-                </CardText>
-                <button className="btn btn-danger btn-sm" onClick={props.deletePost.bind(null, post.id)}>Delete</button>
-                <button className="btn btn-warning btn-sm" onClick={props.showModal.bind(null, post.id)}>Edit</button>  
-              </CardBody>
-            </Card>
-          </Col>
-        </Row>
-        
+      <Col className="column-posts" sm={6}>
+        <Card key={post.id}>
+          <CardImg top width="auto" src={post.img_url} alt="" />
+          <CardBody>
+            <CardTitle>
+              <h3 className="blog-title">{post.title}</h3>
+            </CardTitle>
+            <CardSubtitle>
+                <h6>{post.date}</h6>
+            </CardSubtitle>
+            <CardText className="blog-body">
+              {post.body}
+            </CardText>
+            <button className="btn btn-danger btn-sm" onClick={props.deletePost.bind(null, post.id)}>Delete</button>
+            <button className="btn btn-warning btn-sm" onClick={props.showModal.bind(null, post.id)}>Edit</button>  
+          </CardBody>
+          <Comments postID={post.id} comments={props.comments} deleteComment={props.deleteComment} showCommentModal={props.showCommentModal} />
+          <CreateComment postID={'http://localhost:8000/api/posts/' + post.id + '/'} addComment={props.addComment} />
+        </Card>
+      </Col>
+      
 
-        <Comments postID={post.id} comments={props.comments} deleteComment={props.deleteComment} showCommentModal={props.showCommentModal} />
-        <CreateComment postID={'http://localhost:8000/api/posts/' + post.id + '/'} addComment={props.addComment} />
-      </Container>
+      
 
     )
   })
@@ -53,10 +47,13 @@ const Posts = (props) => {
       
       
     return (
-
-    <ul>
-      {postList}
-    </ul>
+      <Container className="container">
+        <Row>
+          <CardDeck>
+            {postList}
+          </CardDeck>
+        </Row>
+      </Container>  
   )
 
 };
